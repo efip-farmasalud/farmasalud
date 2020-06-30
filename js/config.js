0769f3ac822b0eb829a67de06fc04d651cc89d49
Matias Gonzalo calvo <matiasgonzalocalvo@gmail.com>
2020-06-30 00:57:09 -0300

    --- Auto-Git Commit ---

--- js/config.js
+++ js/config.js
@@ -2,12 +2,12 @@ class configuration
 {
   constructor()
   {
-    this.urlApiSucursales = "http://localhost:9080/";
-    this.urlArticulos = "http://localhost:9080/";
-    this.urlInventario = "http://localhost:9080/inventory/";
-    this.urlLogin = "http://localhost:9080/login/";
-    this.urlUsers = "http://localhost:9080/user/";
-    this.logut_url = "http://localhost:9080/logout/localhost/farmasalud";
+    this.urlApiSucursales = "https://farmasaludapi.mgcalvo.com/";
+    this.urlArticulos = "https://farmasaludapi.mgcalvo.com/";
+    this.urlInventario = "https://farmasaludapi.mgcalvo.com/inventory/";
+    this.urlLogin = "https://farmasaludapi.mgcalvo.com/login/";
+    this.urlUsers = "https://farmasaludapi.mgcalvo.com/user/";
+    this.logut_url = "https://login.mgcalvo.com/logout/localhost/farmasalud";
     this.id_menu_usuario = "menu-usario";
     this.id_email = "email";
     this.id_logout = "logout";
